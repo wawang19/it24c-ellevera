8813c30e323fdc9eec41494cd35db6cf2d542c54
wawang19 <ellevera.oscarjay19@gmail.com>
2024-11-06 14:46:27 +0800

    Added API keys, Text Input, Geolocation, and Weather Card

---
+++ Applet-5/applet-5.js
@@ -0,0 +1,21 @@
+class WeatherApp {
+    constructor() {
+        // API Key
+        this.apiKeyInput = document.getElementById('apiKeyInput'); 
+
+        // Text Input
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+
+        // Geolocation Input
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+
+        // Weather Card
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+
+      
