98f1811f7d1f68cc767b8b135dab0c2350952f7f
wawang19 <ellevera.oscarjay19@gmail.com>
2024-11-07 08:42:24 +0800

    Added done task

--- Applet 6/applet-6.js
+++ Applet 6/applet-6.js
@@ -36,4 +36,13 @@ class TodoList {
     `;
     this.todoList.appendChild(listItem);
   
+  }
   
+  doneTask(event) {
+    const taskItem = event.target.closest('.todo-item');
+    const taskText = taskItem.querySelector('.task-text');
+    taskText.classList.toggle('completed'); 
+  
+    const buttons = taskItem.querySelectorAll('button');
+    buttons.forEach(button => button.disabled = true);
+  }
