34eb4a757f168c164e5c484c7598375630b21018
wawang19 <ellevera.oscarjay19@gmail.com>
2024-11-07 08:40:02 +0800

    Added an add or update task

--- Applet 6/applet-6.js
+++ Applet 6/applet-6.js
@@ -14,4 +14,12 @@ class TodoList {
       });
   }
   
+  addOrUpdateTask() {
+    const taskText = this.todoInput.value.trim();
+    if (taskText) {
+        this.editingIndex === -1 ? this.addTask(taskText) : this.updateTask(taskText);
+        this.todoInput.value = '';
+    }
+  }
+  
   
